9376ec36d9b036e41d9e54ec578d90eacb3535f4
shraddha0930 <shraddhaingole11@gmail.com>
2023-06-22 13:26:07 +0530

    Added:code to find sum of command line arguments and count the invalid integer entered

---
+++ SumOfCommandlineArgument.java
@@ -0,0 +1,18 @@
+package day1assignment;
+
+public class SumOfCommandlineArgument {
+    public static void main(String a[]) {
+        int sum = 0;
+        int invalid = 0;
+        for (int I = 0; I < a.length; I++) {
+            try {
+                sum += Integer.parseInt(a[I]);
+            } catch (NumberFormatException e) {
+                invalid++;
+            }
+        }
+        System.out.println("Total no.of arguments: " +a.length);
+        System.out.println("Invalid Integers:"  +invalid);
+        System.out.println("Sum:" +sum);
+    }
+}
