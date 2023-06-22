68723f2dd057c3186ab8e71959c4be1126011f69
shraddha0930 <shraddhaingole11@gmail.com>
2023-06-22 13:23:20 +0530

    Added:code to display the default value of all primitive data types of java

---
+++ PrimitiveDataType.java
@@ -0,0 +1,24 @@
+package day1assignment;
+
+public class PrimitiveDataType {
+    static byte b;
+    static short s;
+    static char c;
+    static float f;
+    static double d;
+    static int i;
+    static long l;
+    static boolean t;
+
+    public static void main(String[] args) {
+        System.out.println("Default value of byte Type = " +b);
+        System.out.println("Default value of short Type = " +s);
+        System.out.println("Default value of char Type = " +c);
+        System.out.println("Default value of float Type = " +f);
+        System.out.println("Default value of double Type = " +d);
+        System.out.println("Default value of int Type = " +i);
+        System.out.println("Default value of long Type = " +l);
+        System.out.println("Default value of boolean Type = " +t);
+
+    }
+}
