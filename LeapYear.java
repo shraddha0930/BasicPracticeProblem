74d8f35599935cf1617e8550d1d1f24a2410c644
shraddha0930 <shraddhaingole11@gmail.com>
2023-06-22 13:31:41 +0530

    Added:code to check leap year or not

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package day1assignment;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year:");
+        int year = scanner.nextInt();
+
+        if (((year % 4 == 0) && (year % 100 != 0))|| (year % 400 == 0)){
+            System.out.println("Leap year");
+        }
+        else
+            System.out.println("Not a Leap Year");
+    }
+}
